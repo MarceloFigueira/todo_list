46a7cf27f0ff3547e5d6743eef0d3a387e330e90
Marcelo Figueira <marcelofigueira87@gmail.com>
2022-11-04 22:06:28 -0300

    create checkbox

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,6 +16,20 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('TO DO'),
         elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          ToDoTile(
+            taskName: "Tomar Cafe",
+            taskCompleted: true,
+            onChanged: (p0) {},
+          ),
+          ToDoTile(
+            taskName: "Ler um livro",
+            taskCompleted: false,
+            onChanged: (p0) {},
+          ),
+        ],
       ),
     );
   }
--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({Key? key}) : super(key: key);
+  ToDoTile({
+    Key? key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final String taskName;
+  final bool taskCompleted;
+
+  Function(bool?)? onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+            color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          children: [
+            //checkbox
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+
+            //task name
+            Text(taskName),
+          ],
+        ),
+      ),
+    );
   }
 }
